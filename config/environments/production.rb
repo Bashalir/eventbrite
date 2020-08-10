262b0cf50f6209215ec18750fdad9ea710c510a5
Bashalir <bachir@sahali.fr>
2020-08-10 15:03:51 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,18 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.action_mailer.default_url_options = { :host => "portal.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => ENV["MAIL_LOGIN"],
+    :password => ENV["MAIL_PWD"],
+    :authentication => "login",
+    :enable_starttls_auto => true,
+  }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
