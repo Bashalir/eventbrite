48687c2623041fd4a3710280eecfbef15e5cf42b
Bashalir <bachir@sahali.fr>
2020-08-10 15:07:09 +0200

    fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :domain => "google.com", #you can also use google.com
-  :authentication => :plain,
-  :enable_starttls_auto => true,
-  :user_name => ENV["MAIL_LOGIN"],
-  :password => ENV["MAIL_PWD"],
-}
